513998dbace843e71b8dfdfd1aae699abdddaecf
Kanjiu Akuma <18683538+joscha1996@users.noreply.github.com>
2018-12-21 23:07:37 +0100

    Added lua module for easier including

---
+++ j_app.lua
@@ -0,0 +1,134 @@
+m = {}
+
+local m_uuid = "df766ed3-e589-48a1-a889-5dd16787f0bb"
+
+function m.createHere()
+	project "JApp"
+		location "JApp"
+		language "C++"
+		cppdialect "C++17"
+		systemversion "latest"
+		uuid(m_uuid)
+		kind "StaticLib"
+
+		targetdir(bin_dir)
+		objdir(bin_int_dir)
+
+		files 
+		{
+			"%{prj.name}/src/**.h",
+			"%{prj.name}/src/**.cpp",
+		}
+
+		includedirs {
+			"%{prj.name}/src",
+			"%{prj.name}/src/vendor",
+			"dependencies/GLEW/include",
+			"dependencies/GLFW/include",
+		}
+
+		libdirs {
+			"dependencies/GLFW/lib",
+			"dependencies/GLEW/lib",
+		}
+
+		links {
+			"glfw3.lib",
+			"opengl32",
+			"glew32s.lib",
+		}
+
+		filter "system:windows"
+			defines {
+				"APP_PLATFORM_WINDOWS"
+			}
+
+		filter "configurations:Debug"
+			symbols "On"
+			defines {
+				"APP_DEBUG"
+			}
+
+		filter "configurations:Release"
+			optimize "On"
+			defines {
+				"APP_RELEASE"
+			}
+
+		filter "configurations:Dist"
+			optimize "On"
+			defines {
+				"APP_DIST"
+			}
+		filter {}
+end
+
+function m.includeHere(j_app_path)
+externalproject "JApp"
+	location(j_app_path .. "/JApp")
+	uuid(m_uuid)
+	language "C++"
+	cppdialect "C++17"
+	systemversion "latest"
+	kind "StaticLib"
+end
+
+function m.configureThis(j_app_path)
+	filter "configurations:Debug"
+		kind "ConsoleApp"
+
+	filter "configurations:Release"
+		kind "ConsoleApp"
+
+	filter "configurations:Dist"
+		kind "WindowedApp"
+		entrypoint "mainCRTStartup"
+	
+	filter {}
+
+	includedirs {
+		j_app_path .. "/dependencies/GLEW/include",
+		j_app_path .. "/dependencies/GLFW/include",
+		j_app_path .. "/JApp/src",
+	}
+
+	libdirs {
+		j_app_path .. "/dependencies/GLFW/lib",
+		j_app_path .. "/dependencies/GLEW/lib",
+	}
+
+	links {
+		"glfw3.lib",
+		"opengl32",
+		"glew32s.lib",
+		"JApp",
+	}
+
+	filter "system:windows"
+		defines {
+			"APP_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		targetsuffix ("-dbg")
+		symbols "On"
+		defines {
+			"APP_DEBUG"
+		}
+
+	filter "configurations:Release"
+		targetsuffix ("-rls")
+		optimize "On"
+		defines {
+			"APP_RELEASE"
+		}
+
+	filter "configurations:Dist"
+		optimize "On"
+		defines {
+			"APP_DIST"
+		}
+	filter {}
+end
+
+return m;
--- premake5.lua
+++ premake5.lua
@@ -2,10 +2,12 @@ out_dir = "%{wks.location}/build/"
 bin_dir = out_dir .. "bin/%{prj.name}/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 bin_int_dir = out_dir .. "bin-int/%{prj.name}/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+j_app = require("j_app")
+
 workspace "JApp"
 	architecture "x64"
 	characterset ("MBCS")
-	startproject "Sandbox"
+	startproject "Test"
 
 	configurations
 	{
@@ -14,5 +16,29 @@ workspace "JApp"
 		"Dist",
 	}
 
-include("JApp/premake5.lua")
-include("Test/premake5.lua")
+	defines {
+		"GLEW_STATIC"
+	}
+
+j_app.createHere()
+
+project "Test"
+	location "Test"
+	language "C++"
+	cppdialect "C++17"
+	systemversion "latest"
+	
+	j_app.configureThis("%{wks.location}")
+
+	targetdir(bin_dir)
+	objdir(bin_int_dir)
+
+	includedirs {
+		"src",
+	}
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
