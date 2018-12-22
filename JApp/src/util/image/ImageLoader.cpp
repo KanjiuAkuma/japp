b7f485426cac7fb9cc19569bd3bdea45339f2c1c
Kanjiu Akuma <18683538+joscha1996@users.noreply.github.com>
2018-12-22 15:48:44 +0100

    Added image loader

---
+++ JApp/src/util/image/ImageLoader.cpp
@@ -0,0 +1,43 @@
+﻿#include "ImageLoader.h"
+
+ImageLoader::ImageLoader(const std::string& path) : m_path(path) {}
+ImageLoader::~ImageLoader() {
+	stbi_image_free(m_image);
+}
+
+void ImageLoader::start() {
+	m_thread = new std::thread(&ImageLoader::load, this);
+}
+
+void ImageLoader::load() {
+	// load image
+	stbi_set_flip_vertically_on_load(1);
+	m_image = stbi_load(m_path.c_str(), &m_imageWidth, &m_imageHeight, &m_bitsPerPixel, 4);
+	m_isFinished = true;
+}
+
+void ImageLoader::join() const {
+	if (m_thread && !m_isFinished) {
+		m_thread->join();
+	}
+}
+
+bool ImageLoader::isFinished() const {
+	return m_isFinished;
+}
+
+unsigned char* ImageLoader::getImage() const {
+	return m_image;
+}
+
+int ImageLoader::getImageWidth() const {
+	return m_imageWidth;
+}
+
+int ImageLoader::getImageHeight() const {
+	return m_imageHeight;
+}
+
+int ImageLoader::getBitsPerPixel() const {
+	return m_bitsPerPixel;
+}
---
+++ JApp/src/util/image/ImageLoader.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <string>
+#include "stb/stb_image.h"
+#include <thread>
+
+class ImageLoader {
+private:
+	std::string m_path;
+	unsigned char* m_image = nullptr;
+	int m_imageWidth = 0, m_imageHeight = 0, m_bitsPerPixel = 0;
+	bool m_isFinished = false;
+	std::thread* m_thread = nullptr;
+public:
+	explicit ImageLoader(const std::string& path);
+	~ImageLoader();
+
+	void start();
+	void load();
+	void join() const;
+
+	bool isFinished() const;
+
+	unsigned char* getImage() const;
+	int getImageWidth() const;
+	int getImageHeight() const;
+	int getBitsPerPixel() const;
+
+};
