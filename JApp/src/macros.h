4f07c972b80837d2107fe896ab4c09049776a903
Kanjiu Akuma <18683538+joscha1996@users.noreply.github.com>
2018-12-29 22:21:55 +0100

    Fixed GL macro

--- JApp/src/macros.h
+++ JApp/src/macros.h
@@ -1,17 +1,21 @@
 ﻿#pragma once
+#include "logger/Log.h"
+
+#include <GL/glew.h>
+
 
 #if APP_DEBUG
 	#define APP_CONFIGURATION "debug"
 	#define ASSERT(x) if(!(x)) __debugbreak()
 	#define RELEASE(x) x
 	#define DEBUG(x) x
-	#define GL_CALL(x) x
+	#define GL_CALL(x) glClearErrors(); x; ASSERT(glLogCall(#x, __FILE__, __LINE__))
 #elif APP_RELEASE
 	#define APP_CONFIGURATION "release"
 	#define ASSERT(x) if(!(x)) __debugbreak()
 	#define RELEASE(x) x
 	#define DEBUG(x)
-	#define GL_CALL(x) x
+	#define GL_CALL(x) glClearErrors(); x; glLogCall(#x, __FILE__, __LINE__)
 #elif APP_DIST
 	#define APP_CONFIGURATION "dist"
 	#define ASSERT(x)
@@ -26,3 +30,20 @@
 	#define DEBUG(x) x
 	#define GL_CALL(x) x
 #endif
+
+inline void glClearErrors()
+{
+	while (glGetError() != GL_NO_ERROR);
+}
+
+inline bool glLogCall(const char* function, const char* file, long line)
+
+{
+	bool noErr = true;
+	while (GLenum error = glGetError())
+	{
+		APP_CORE_ERROR("[OpenGL Error] {}: in {}.{}:{}\n", error, function, file, line);
+		noErr = false;
+	}
+	return noErr;
+}
