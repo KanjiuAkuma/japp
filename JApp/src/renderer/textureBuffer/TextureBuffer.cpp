f5d65f7d846af5c7f7a3966875831ea3cff9952f
Kanjiu Akuma <18683538+joscha1996@users.noreply.github.com>
2019-01-01 00:37:02 +0100

    Added data type and changed width and height to size

--- JApp/src/renderer/textureBuffer/TextureBuffer.cpp
+++ JApp/src/renderer/textureBuffer/TextureBuffer.cpp
@@ -3,10 +3,33 @@
 #include "macros.h"
 #include <GL/glew.h>
 
-TextureBuffer::TextureBuffer() {
+TextureBuffer::TextureBuffer(const DataType dataType) {
 	GL_CALL(glGenBuffers(1, &m_bufferId));
 	GL_CALL(glBindBuffer(GL_TEXTURE_BUFFER, m_bufferId));
 	GL_CALL(glGenTextures(1, &m_textureId));
+	GL_CALL(glBindBuffer(GL_TEXTURE_BUFFER, 0));
+
+	switch (dataType) {
+
+	case FLOAT:
+		m_dataType = GL_R32F;
+		break;
+	case VEC2:
+		m_dataType = GL_RG32F;
+		break;
+	case VEC3:
+		m_dataType = GL_RGB32F;
+		break;
+	case VEC4:
+		m_dataType = GL_RGBA32F;
+		break;
+	default: ASSERT(false);
+	}
+}
+
+TextureBuffer::TextureBuffer(float* data, const unsigned int size, const DataType dataType) : TextureBuffer(dataType) {
+	GL_CALL(glBindBuffer(GL_TEXTURE_BUFFER, m_bufferId));
+	GL_CALL(glBufferData(GL_TEXTURE_BUFFER, size * sizeof(float), data, GL_STATIC_DRAW));
 	GL_CALL(glBindBuffer(GL_TEXTURE_BUFFER, 0));
 }
 
@@ -15,16 +38,26 @@ TextureBuffer::~TextureBuffer() {
 	GL_CALL(glDeleteTextures(1, &m_textureId));
 }
 
-void TextureBuffer::setData(float* data, const unsigned int width, const unsigned int height) {
+void TextureBuffer::setData(float* data, const unsigned int size) const {
 	GL_CALL(glBindBuffer(GL_TEXTURE_BUFFER, m_bufferId));
-	GL_CALL(glBufferData(GL_TEXTURE_BUFFER, width * height * sizeof(float), data, GL_STATIC_DRAW));
+	GL_CALL(glBufferData(GL_TEXTURE_BUFFER, size * sizeof(float), data, GL_STATIC_DRAW));
 	GL_CALL(glBindBuffer(GL_TEXTURE_BUFFER, 0));
 }
 
+float* TextureBuffer::getDataPointer() const {
+	GL_CALL(glBindBuffer(GL_TEXTURE_BUFFER, m_bufferId));
+	GL_CALL(return (float*) glMapBuffer(GL_TEXTURE_BUFFER, GL_WRITE_ONLY));
+}
+
+void TextureBuffer::freeDataPointer() {
+	GL_CALL(glUnmapBuffer(GL_TEXTURE_BUFFER));
+}
+
 void TextureBuffer::bind(const unsigned int slot) const {
+	ASSERT(slot != 0);
 	GL_CALL(glActiveTexture(GL_TEXTURE0 + slot));
 	GL_CALL(glBindTexture(GL_TEXTURE_BUFFER, m_textureId));
-	GL_CALL(glTexBuffer(GL_TEXTURE_BUFFER, GL_R32F, m_bufferId));
+	GL_CALL(glTexBuffer(GL_TEXTURE_BUFFER, m_dataType, m_bufferId));
 }
 
 void TextureBuffer::unbind() const {
--- JApp/src/renderer/textureBuffer/TextureBuffer.h
+++ JApp/src/renderer/textureBuffer/TextureBuffer.h
@@ -1,11 +1,25 @@
 ﻿#pragma once
 
+#include <GL/glew.h>
+
 class TextureBuffer {
 	unsigned int m_textureId, m_bufferId;
+	GLenum m_dataType;
 public:
-	TextureBuffer();
+
+	enum DataType {
+		FLOAT,
+		VEC2,
+		VEC3,
+		VEC4,
+	};
+
+	explicit TextureBuffer(DataType dataType);
+	explicit TextureBuffer(float* data, unsigned int size, DataType dataType);
 	~TextureBuffer();
-	void setData(float* data, unsigned int width, unsigned int height);
+	void setData(float* data, unsigned int size) const;
+	float* getDataPointer() const;
+	static void freeDataPointer();
 	void bind(unsigned int slot = 0) const;
 	void unbind() const;
 };
